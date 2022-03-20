6713807498d46b51477a6fa130d7097d5a06df73
vrugtehagel <vrugtehagel@gmail.com>
2022-03-20 13:18:16 +0000

    Update minified file and dependency versions

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { define, secret } from './define.js'
 export { define } from './define.js'
 export { default as register } from './register.js'
 
-export { reversible, until } from 'https://deno.land/x/reversibles@v1.0.0/index.js'
-export { default as when } from 'https://deno.land/x/reversibles@v1.0.0/library/when/index.js'
+export { reversible, until } from 'https://deno.land/x/reversibles@v1.1.1/index.js'
+export { default as when } from 'https://deno.land/x/reversibles@v1.1.1/library/when/index.js'
 
 export { default as stateify } from 'https://deno.land/x/stateify@v1.0.0/index.js'
 
