3f47c9f75218daedc87132b1e1d5580cadc32dce
vrugtehagel <vrugtehagel@gmail.com>
2022-03-20 12:02:30 +0000

    Removed test code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export { default as register } from './register.js'
 
 export { reversible, until } from 'https://deno.land/x/reversibles@v1.0.0/index.js'
-// export { default as when } from 'https://deno.land/x/reversibles@v1.0.0/library/when/index.js'
-export { default as when } from '../reversibles/library/when/index.js'
+export { default as when } from 'https://deno.land/x/reversibles@v1.0.0/library/when/index.js'
 
 export { default as stateify } from 'https://deno.land/x/stateify@v1.0.0/index.js'
