e318e88586d2e54f5ea1cdc98e57ff19cae55815
vrugtehagel <vrugtehagel@pm.me>
2023-12-05 17:45:34 +0100

    Improve play console behavior

--- site/-/js/play-console/index.js
+++ site/-/js/play-console/index.js
@@ -1,6 +1,7 @@
 let messenger
 const loading = import('/-/js/context-messenger/index.js')
 	.then(({ContextMessenger}) => messenger = new ContextMessenger(window.parent))
+	.then(() => messenger.ready())
 
 window.onerror = (event, source, line, column, error) => {
 	sendError(source, line, column, error)
@@ -10,14 +11,12 @@ window.onerror = (event, source, line, column, error) => {
 window.addEventListener('unhandledrejection', async event => {
 	event.preventDefault()
 	await loading
-	await messenger.ready()
 	const {message} = event.reason
 	await messenger.send('error', {message})
 })
 
 async function sendError(source, line, column, error){
 	await loading
-	await messenger.ready()
 	const {message} = error
 	const url = new URL(source, location.origin)
 	const src = url.pathname
@@ -27,11 +26,38 @@ async function sendError(source, line, column, error){
 console.log = Object.assign((...args) => {
 	console.log.original.apply(console, args)
 	loading.then(async () => {
-		await messenger.ready()
-		const message = args.map(arg => `${arg}`).join(' ')
+		const message = args.map(arg => prettyPrint(arg)).join(' ')
 		messenger.send('log', {message})
 	})
 }, {original: console.log})
 
 window.ping = () => void loading.then(() => messenger.send('ping'))
 window.pong = () => void loading.then(() => messenger.send('pong'))
+
+function prettyPrint(thing){
+	if(typeof thing == 'number') return `${thing}`
+	if(typeof thing == 'bigint') return `${thing}n`
+	if(typeof thing == 'string') return `"${thing}"`
+	if(typeof thing == 'undefined') return 'undefined'
+	if(typeof thing == 'symbol') return thing.toString()
+	if(typeof thing == 'boolean') return `${thing}`
+	if(typeof thing == 'function'){
+		if(thing.toString().startsWith('class'))
+			return `class ${thing.name} { … }`
+		return `function ${thing.name}(){ … }`
+	}
+	if(thing == null) return 'null'
+	if(thing instanceof Element) return `<${thing.localName}>`
+	if(thing instanceof Node && thing.nodeName) return `${thing.nodeName}`
+	if(Array.isArray(thing)){
+		const preview = thing.slice(0, 3).map(prettyPrint).join(', ')
+		return thing.length <= 3 ? `[ ${preview} ]` : `[ ${preview}, … ]`
+	}
+	if(thing.constructor.name != 'Object') `${thing.constructor.name} { … }`
+	const keys = Reflect.ownKeys(thing)
+	const preview = keys
+		.slice(0, 2)
+		.map(key => `${prettyPrint(key)}: ${prettyPrint(thing[key])}`)
+		.join(', ')
+	return keys.length <= 2 ? `{ ${preview} }` : `{ ${preview}, … }`
+}
