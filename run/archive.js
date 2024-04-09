03b4aef6a5ee4887683791ea1f74a216b3860fa4
vrugtehagel <vrugtehagel@pm.me>
2024-04-09 17:21:25 +0200

    Minor rework of archiving script

--- run/archive.js
+++ run/archive.js
@@ -3,83 +3,90 @@ import { crypto } from 'std/crypto/mod.ts'
 import { encodeBase64 } from 'std/encoding/base64.ts'
 import { build } from './build.js'
 
+
+// This script needs to be called from the CLI.
+// It asks the user a few questions, and then builds & archives a new version.
 if(!import.meta.main)
 	throw Error('The archive script should be run as "deno task archive"')
 
-const print = message => console.log(`${gray('?')} ${message}`)
-print.ok = message => console.log(`${green('✓')} ${message}`)
-print.fail = message => console.log(`${red('✘')} ${message}`)
-
+// Must match with what the build script does
 const lib = 'dist/lib-latest.js'
 const dev = 'dist/dev-latest.js'
+
+const printOk = message => console.log(`${green('✓')} ${message}`)
+const printInfo = message => console.log(`${gray('?')} ${message}`)
+const printFail = message => console.log(`${red('✘')} ${message}`)
 
 await build({noVerify: true})
 const json = await Deno.readTextFile('versions.json')
 const versions = JSON.parse(json)
-const changelog = {}
-const timestamp = Date.now()
+
 const version = {
 	number: null,
 	hash: null,
-	timestamp,
-	size: {},
-	changelog
+	timestamp: Date.now(),
+	size: {gzip: -1, brotli: -1},
+	changelog: {breaking: [], improvements: [], bugs: []}
 }
 
-print('Let\'s go over the changes.\n')
-print('Leave empty to move on.')
+printInfo('Let\'s go over the changes.\n')
+printInfo('Leave empty to move on.')
 
-let change
-changelog.breaking = []
-while(change = prompt('  Breaking change:')) changelog.breaking.push(change)
-changelog.improvements = []
-while(change = prompt('  Improvement:')) changelog.improvements.push(change)
-changelog.bugs = []
-while(change = prompt('  Bug fixed:')) changelog.bugs.push(change)
+await (async () => {
+	const {breaking, improvements, bugs} = version.changelog
+	let change
+	while(change = prompt('  Breaking change:')) breaking.push(change)
+	while(change = prompt('  Improvement:')) improvements.push(change)
+	while(change = prompt('  Bug fixed:')) bugs.push(change)
+})()
 
-const args = ['--stdout', lib]
-const gzip = await new Deno.Command('gzip', {args}).output()
-	.then(({stdout}) => stdout.length)
-const brotli = await new Deno.Command('brotli', {args}).output()
-	.then(({stdout}) => stdout.length)
-version.size = {gzip, brotli}
-const sizeStatus = gzip < 5000 ? 'ok' : 'fail'
-print[sizeStatus](`Gzipped: ${gray(gzip.toString())} bytes`)
-print.ok(`Brotli'd: ${gray(brotli.toString())} bytes`)
+await (async () => {
+	const args = ['--stdout', lib]
+	const gzip = await new Deno.Command('gzip', {args}).output()
+		.then(({stdout}) => stdout.length)
+	const brotli = await new Deno.Command('brotli', {args}).output()
+		.then(({stdout}) => stdout.length)
+	Object.assign(version.size, {gzip, brotli})
+	const print = gzip < 5000 ? printOk : printFail
+	print(`Gzipped: ${gray(gzip.toString())} bytes`)
+	printOk(`Brotli'd: ${gray(brotli.toString())} bytes`)
+})()
 
-const bytes = await Deno.readFile(lib)
-const hash = encodeBase64(await crypto.subtle.digest('MD5', bytes))
-version.hash = hash
-const hashUnique = !versions.some(version => version.hash == hash)
-if(hashUnique) print.ok(`Hash: ${gray(hash)}`)
-else print.fail(`Hash (not unique): ${gray(hash)}`)
+await (async () => {
+	const bytes = await Deno.readFile(lib)
+	const hash = encodeBase64(await crypto.subtle.digest('MD5', bytes))
+	version.hash = hash
+	const isUnique = !versions.some(version => version.hash == hash)
+	if(isUnique) printOk(`Hash: ${gray(hash)}`)
+	else printFail(`Hash (not unique): ${gray(hash)}`)
+})()
 
-const [main, major, minor] = versions[0].number.split('.')
-	.map(number => Number(number))
-let versionNumber = changelog.breaking.length > 0
-	? [main, major + 1, 0].join('.')
-	: [main, major, minor + 1].join('.')
-print(`Guessing version ${gray(versionNumber)}`)
-const guessedCorrect = confirm('  Continue?')
-if(!guessedCorrect) while(true){
-	versionNumber = prompt('  Enter version number:')
-	const exists = versions.some(version => version.number == versionNumber)
-	if(!exists) break
-	print.fail(`Version "${versionNumber}" already exists.`)
-}
-version.number = versionNumber
+await (async () => {
+	const [main, major, minor] = versions[0].number.split('.')
+		.map(number => Number(number))
+	const guess = version.changelog.breaking.length > 0
+		? [main, major + 1, 0].join('.')
+		: [main, major, minor + 1].join('.')
+	printInfo(`Guessing version ${gray(guess)}`)
+	const correct = confirm('  Continue?')
+	if(correct) return version.number = guess
+	let number
+	while(true){
+		number = prompt('  Enter version number:')
+		const exists = versions.some(version => version.number == number)
+		if(number && !exists) break
+		printFail(`Version "${versionNumber}" already exists.`)
+	}
+	version.number = number
+})()
 
-const sure = confirm('  Are you sure you want to archive this new version?')
-if(!sure){
-	print.fail('Operation cancelled. Exiting…')
-	Deno.exit(0)
-}
-
-await Deno.copyFile(lib, `archive/lib-${version.number}.js`)
-await Deno.copyFile(dev, `archive/dev-${version.number}.js`)
-
-versions.unshift(version)
-const stringified = JSON.stringify(versions, null, 4)
-await Deno.writeTextFile('versions.json', stringified)
-
-print.ok(`Version ${version.number} archived successfully.`)
+await (async () => {
+	const sure = confirm('  Are you sure you want to archive a new version?')
+	if(!sure) return printFail('Operation cancelled. Exiting…')
+	await Deno.copyFile(lib, `archive/lib-${version.number}.js`)
+	await Deno.copyFile(dev, `archive/dev-${version.number}.js`)
+	versions.unshift(version)
+	const stringified = JSON.stringify(versions, null, 4)
+	await Deno.writeTextFile('versions.json', stringified)
+	printOk(`Version ${version.number} archived successfully.`)
+})()
