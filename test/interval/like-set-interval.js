9278b6b3e8866c50ba0c77d576e5f685cb9398fb
vrugtehagel <vrugtehagel@pm.me>
2024-04-14 13:06:33 +0200

    Fix tests & minor update to readme

--- test/interval/like-set-interval.js
+++ test/interval/like-set-interval.js
@@ -1,16 +1,16 @@
 const { interval } = self.yozo;
 
 let triggers = 0;
-const flow = interval(40).then(() => {
+const flow = interval(100).then(() => {
 	triggers++;
 });
 
 assert(triggers == 0);
 
-await new Promise((resolve) => setTimeout(resolve, 60));
+await new Promise((resolve) => setTimeout(resolve, 150));
 assert(triggers == 1);
 
-await new Promise((resolve) => setTimeout(resolve, 80));
+await new Promise((resolve) => setTimeout(resolve, 200));
 assert(triggers == 3);
 
 flow.stop();
