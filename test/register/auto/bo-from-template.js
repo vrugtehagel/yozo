4849464cd39f2e7a39e9b7a86ea150c3f537c36a
vrugtehagel <vrugtehagel@pm.me>
2024-04-28 16:28:53 +0200

    Add tests for register.auto()

---
+++ test/register/auto/bo-from-template.js
@@ -0,0 +1,19 @@
+const template = document.createElement('template');
+template.innerHTML = `
+	<template>
+		<yet-undefined-from-template></yet-undefined-from-template>
+	</template>
+`;
+document.body.append(template);
+
+let triggers = 0
+let attempt
+window.yozo.register.auto(name => {
+	triggers++
+	attempt = name
+});
+
+assert(triggers == 1);
+assert(attempt == 'yet-undefined-from-template')
+
+export const refresh = true
