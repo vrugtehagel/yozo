e91f193b36a1e8a5e2ea67dd9045ebb6e700ece1
vrugtehagel <vrugtehagel@pm.me>
2024-05-26 17:00:00 +0200

    Add tests for live.delete()

---
+++ test/live/delete/not-monitored.js
@@ -0,0 +1,17 @@
+const { live, monitor, until } = self.yozo;
+
+const $ = live({ foo: 23 });
+const call = monitor(['live'], async () => {
+	await until('microtask');
+	live.delete($.$foo);
+});
+
+let triggers = 0;
+call.live.addEventListener('change', () => {
+	triggers++;
+});
+
+await 'microtask';
+$.foo = -1;
+
+assert(triggers == 0);
---
+++ test/live/delete/return-value.js
@@ -0,0 +1,12 @@
+const { live } = self.yozo;
+
+const object = { foo: 23 }
+Object.defineProperty(object, 'qux', { value: 7 });
+
+const $ = live(object);
+assert(live.delete($.$foo) == true);
+assert(live.delete($) == true);
+assert(live.delete($.$qux) == false);
+assert(live.delete($.$bar.$baz) == false);
+assert(live.delete(null) == false);
+assert(live.delete({}) == false);
