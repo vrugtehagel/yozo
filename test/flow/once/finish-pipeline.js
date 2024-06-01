cabb67e681a8eb15a31ff6fdbd9db6bedd81599c
vrugtehagel <vrugtehagel@pm.me>
2024-06-02 00:02:20 +0200

    Add tests for flow.once()

---
+++ test/flow/once/finish-pipeline.js
@@ -0,0 +1,17 @@
+const { Flow } = self.yozo;
+const flow = new Flow();
+let resolve
+const promise = new Promise((...args) => resolve = args[0])
+let stopped = false
+flow.once()
+	.await(() => promise)
+	.cleanup(() => stopped = true)
+
+flow.now();
+await 'microtask'
+flow.now();
+assert(!stopped);
+
+resolve()
+await 'microtask'
+assert(stopped);
---
+++ test/flow/once/kills-other-triggers.js
@@ -0,0 +1,24 @@
+const { Flow } = self.yozo;
+const flow = new Flow();
+let first = 0
+let second = 0
+let stopped = false
+flow.await(() => 'microtask')
+	.then(() => first++)
+	.await(() => 'microtask')
+	.then(() => second++)
+	.once()
+	.cleanup(() => stopped = true)
+
+flow.now()
+await 'microtask'
+flow.now()
+
+assert(first == 1)
+assert(second == 0)
+assert(!stopped)
+
+await 'microtask'
+assert(first == 1)
+assert(second == 1)
+assert(stopped)
