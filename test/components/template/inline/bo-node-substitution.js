89a1ec0dd87f5099abecb9f0f78a91d478f1b05b
vrugtehagel <vrugtehagel@pm.me>
2025-02-21 23:36:41 +0100

    Formatting

--- test/components/template/inline/bo-node-substitution.js
+++ test/components/template/inline/bo-node-substitution.js
@@ -1,21 +1,21 @@
 await self.yozo.register('./bo-node-substitution.yz');
 const element = document.createElement('greeter-node-substitution');
 
-const span = document.createElement('span')
-const em = document.createElement('em')
-span.textContent = 'hello'
-em.textContent = 'world'
+const span = document.createElement('span');
+const em = document.createElement('em');
+span.textContent = 'hello';
+em.textContent = 'world';
 element.greeting = span;
 element.receiver = em;
 document.body.append(element);
 await 'microtask';
 assert(element.textContent.trim() == 'hello, world!');
 
-element.greeting = 'hi'
-em.textContent = 'earth'
+element.greeting = 'hi';
+em.textContent = 'earth';
 await 'microtask';
 assert(element.textContent.trim() == 'hi, earth!');
 
-element.greeting = span
+element.greeting = span;
 await 'microtask';
 assert(element.textContent.trim() == 'hello, earth!');
