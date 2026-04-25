7bda626576f07f16b38df6b63442ee9dd4169b24
vrugtehagel <vrugtehagel@pm.me>
2026-04-25 20:15:46 +0200

    Remove console log from test

--- test/components/meta/attribute/bo-live-attributes.js
+++ test/components/meta/attribute/bo-live-attributes.js
@@ -3,17 +3,15 @@ await self.yozo.register('./bo-live-attributes.yz');
 const element = document.createElement('mood-live-attributes');
 document.body.append(element);
 
-
 const call = monitor(['live'], () => {
-	element.mood = 'happy'
-})
-console.log(call.live.cah)
-
-let changes = 0
-call.live.addEventListener('change', () => {
-	changes++
+	element.mood = 'happy';
 });
 
-element.mood = 'sad'
+let changes = 0;
+call.live.addEventListener('change', () => {
+	changes++;
+});
 
-assert(changes == 0)
+element.mood = 'sad';
+
+assert(changes == 0);
